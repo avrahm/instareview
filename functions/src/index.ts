0b27a2718581e76a1111da8286bdd5c0caed8777
Avrahm <avrahmk@me.com>
2022-02-13 19:22:24 -0500

    new put route to update account

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 import express, { Request, Response } from "express";
 import * as functions from "firebase-functions";
-import admin from "./fb-admin";
+import { db } from "./firebase";
 
 const app = express();
 
@@ -9,7 +9,6 @@ const app = express();
 // Allow any domain that requests a resource from my server
 app.use(cors({ origin: true }));
 
-const db = admin.firestore();
 
 // Routes
 app.get("/hello-world", (req: Request, res: Response) => {
@@ -18,15 +17,16 @@ app.get("/hello-world", (req: Request, res: Response) => {
 
 // Create Account
 // Post
-app.post("/api/create", async (req: Request, res: Response) => {
+app.post("/api/account", async (req: Request, res: Response) => {
     try {
         await db.collection("accounts").doc().create({
             username: req.body.username,
             platform: req.body.platform,
-            createdAt: admin.firestore.FieldValue.serverTimestamp(),
-            lastUpdatedAt: admin.firestore.FieldValue.serverTimestamp(),
+            createdAt: new Date(),
+            lastUpdatedAt: new Date(),
             createdBy: req.body.createdBy || "",
             realname: req.body.realname || "",
+            overallrating: req.body.overallrating || 0,
         });
         return res.status(200).send("Account created!")
     } catch (error) {
@@ -41,7 +41,6 @@ app.get("/api/account/:id", async (req: Request, res: Response) => {
         const document = db.collection("accounts").doc(req.params.id);
         const account = await document.get();
         const response = account.data();
-
         return res.status(200).send(response)
     } catch (error) {
         return res.status(500).send(error)
@@ -54,7 +53,6 @@ app.get("/api/accounts", async (req: Request, res: Response) => {
     try {
         const query = db.collection("accounts");
         const response: FirebaseFirestore.DocumentData[] = [];
-
         await query.get().then(snapshot => {
             const docs = snapshot.docs; // the result of the query
             docs.forEach(doc => {
@@ -65,7 +63,6 @@ app.get("/api/accounts", async (req: Request, res: Response) => {
                 response.push(eachDoc);
             });
         });
-
         return res.status(200).send(response)
     } catch (error) {
         return res.status(500).send(error)
@@ -74,6 +71,22 @@ app.get("/api/accounts", async (req: Request, res: Response) => {
 
 // Update
 // Put
+app.put("/api/account/:id", async (req: Request, res: Response) => {
+    try {
+        const document = db.collection("accounts").doc(req.params.id);
+        await document.update({
+            username: req.body.username,
+            platform: req.body.platform,
+            lastUpdatedAt: new Date(),
+            realname: req.body.realname || "",
+        });
+        return res.status(200).send("Account updated!")
+    }
+    catch (error) {
+        return res.status(500).send(error)
+    }
+});
+
 
 // Delete
 
